eac03c8b8a357f1fdcb5365e1e6c8b9f8ba18203
Mahisa Ahadi <mahisa.academic@gmail.com>
2026-07-28 15:49:51 +0300

    customerController is added

--- ECommerce/Controllers/CartItemController.cs
+++ ECommerce/Controllers/CartItemController.cs
@@ -1,12 +1,110 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerce.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.Controllers
 {
     public class CartItemController : Controller
     {
+        private readonly AppDbContext _context;
+        public CartItemController(AppDbContext context)
+        {
+            _context = context;
+        }
+        //select all
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var cartItem = _context.CartItem.ToList();
+            return View(cartItem);
         }
+
+        //select one
+        [HttpGet]
+        public IActionResult Details(Guid id)
+        {
+            var item = _context.CartItem.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return View(item);
+
+        }
+
+        //update
+        [HttpGet]
+        public IActionResult Edit(Guid id)
+        {
+            var item = _context.CartItem.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return View(item);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(CartItem cartItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.CartItem.Update(cartItem);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cartItem);
+        }
+
+        //delete
+        [HttpGet]
+        public IActionResult Delete(Guid id)
+        {
+            var item = _context.CartItem.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return View(item);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(CartItem cartItem)
+        {
+            if (ModelState.IsValid)
+            {
+                 _context.CartItem.Remove(cartItem);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cartItem) ;
+        }
+
+
+        //insert
+        [HttpGet]
+        public IActionResult Create(Guid id)
+        {
+            var cartItems = _context.CartItem.Find(id);
+            if (cartItems == null)
+            {
+                return NotFound();
+            }
+            return View(cartItems);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CartItem cartItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.CartItem.Add(cartItem);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cartItem);
+        }
+
+
+       
     }
 }
---
+++ ECommerce/Controllers/CustomerController.cs
@@ -0,0 +1,103 @@
+﻿using ECommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CustomerController (AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //select all
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var customer = _context.Customer.ToList();
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        //select one
+        [HttpGet]
+        public IActionResult Details(Guid id)
+        {
+            var customer = _context.Customer.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        //update
+        [HttpGet]
+        public IActionResult Edit(Guid id)
+        {
+            var customer = _context.Customer.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                 _context.Customer.Update(customer);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+        //insert
+        [HttpGet]
+        public IActionResult Create()
+        { return View(); }
+
+        [HttpPost]
+        public IActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Customer.Add(customer);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+        //Delete
+        [HttpGet]
+        public IActionResult Delete(Guid id)
+        {
+            var customer = _context.Customer.Find(id);
+            if (customer == null)
+                return NotFound();
+            return View(customer);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Customer customer)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Customer.Remove(customer);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+    }
+}
